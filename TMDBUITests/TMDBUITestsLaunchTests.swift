91a44296ad1f782821e26188de911427e15f45e9
jorgeramos2 <jorgeramos.1996@outlook.com>
2025-06-02 22:00:25 -0600

    remove default test cases

--- TMDBUITests/TMDBUITestsLaunchTests.swift
+++ TMDBUITests/TMDBUITestsLaunchTests.swift
@@ -7,24 +7,4 @@
 
 import XCTest
 
-final class TMDBUITestsLaunchTests: XCTestCase {
 
-    override class var runsForEachTargetApplicationUIConfiguration: Bool {
-        true
-    }
-
-    override func setUpWithError() throws {
-        continueAfterFailure = false
-    }
-
-    @MainActor
-    func testLaunch() throws {
-        let app = XCUIApplication()
-        app.launch()
-
-        let attachment = XCTAttachment(screenshot: app.screenshot())
-        attachment.name = "Launch Screen"
-        attachment.lifetime = .keepAlways
-        add(attachment)
-    }
-}
